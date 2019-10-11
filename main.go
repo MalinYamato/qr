6d4756e8204b365a94906ec633a508eecffacef5
MalinYamato <malin@yamato.xyz>
2019-10-11 20:43:37 +0200

    added GUI

--- main.go
+++ main.go
@@ -155,7 +155,7 @@ func NewMux(config *Config) *http.ServeMux {
 
 	mux.HandleFunc("/", serveHome)
 	mux.Handle("/session/", requireLogin(http.HandlerFunc(sessionHandler)))
-	mux.Handle("/CouponManager", http.HandlerFunc(PaymentCouponHandler))
+	mux.HandleFunc("/CouponManager", PaymentCouponHandler)
 	mux.Handle("/CreateCoupon", requireLogin(http.HandlerFunc(CreateCouponHandler)))
 	mux.Handle("/GetAllCoupons", requireLogin(http.HandlerFunc(GetAllCouponsHandler)))
 	mux.Handle("/GetCoupon", requireLogin(http.HandlerFunc(GetCouponHandler)))
